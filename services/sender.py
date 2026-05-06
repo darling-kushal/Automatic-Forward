c78f722c56fc27b758b051580dd217a1e7ab2828
darling-kushal <meprabhas01@gmail.com>
2026-05-06 08:22:07 +0530

    Add broadcast_to_groups function for group messaging
    Implement asynchronous broadcasting to groups using Pyrogram.

--- services/sender.py
+++ services/sender.py
@@ -1,1 +1,39 @@
+import asyncio
 
+from pyrogram import Client
+from config import API_ID, API_HASH
+
+async def broadcast_to_groups(user_data, text):
+
+    session = user_data["session"]
+
+    app = Client(
+        name=f"sessions/{user_data['user_id']}",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=session,
+        in_memory=False
+    )
+
+    await app.start()
+
+    async for dialog in app.get_dialogs():
+
+        try:
+            chat = dialog.chat
+
+            if str(chat.type) in ["ChatType.GROUP", "ChatType.SUPERGROUP"]:
+
+                await app.send_message(
+                    chat.id,
+                    text
+                )
+
+                print(chat.title)
+
+                await asyncio.sleep(10)
+
+        except Exception as e:
+            print(e)
+
+    await app.stop()
