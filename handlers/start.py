5651416ae5b7af6cd6ec37e02d70cf29532014c3
darling-kushal <meprabhas01@gmail.com>
2026-05-06 08:19:37 +0530

    Add start command handler for promotion bot

--- handlers/start.py
+++ handlers/start.py
@@ -1,1 +1,8 @@
+from pyrogram import Client, filters
 
+@Client.on_message(filters.command("start"))
+async def start_handler(client, message):
+
+    await message.reply_text(
+        "Welcome to the opt-in promotion bot."
+    )
